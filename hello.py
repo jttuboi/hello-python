b4cb1ff5de9d90a30190aa591ac39f215a167b4f
JonatanFP <jo_fp@yahoo.com.br>
2020-04-02 01:33:40 -0300

    novo numero aparecendo

--- hello.py
+++ hello.py
@@ -1,2 +1,22 @@
 print("Hello World - My first test")
 print("did this f**** work now?")
+
+n=4
+x=3
+a=2
+resp=x
+
+while a <= n:
+    exp = x
+    b=1
+    
+    while b < a:
+        exp=exp*x
+        b=b+1
+    else:
+        resp=resp+exp
+        a=a+2
+        
+
+print (resp)
+        
